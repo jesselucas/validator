84ee2c639543a6291f5168f7903d2d41490e1067
Jesse Lucas <jesse@forestgiant.com>
2016-03-16 15:47:28 -0400

    Adding comments

--- validator.go
+++ validator.go
@@ -7,9 +7,11 @@ import (
 )
 
 var (
+	// Regular expression to test if a URL is valid
 	rxURL = regexp.MustCompile(`\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))`)
 )
 
+// IsUrl test if the rxURL regular expression matches a string
 func IsURL(s string) bool {
 	if s == "" || len(s) >= 2083 || len(s) <= 10 || strings.HasPrefix(s, ".") {
 		return false
